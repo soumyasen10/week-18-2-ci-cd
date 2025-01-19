b0b4ab4ca7ef154816536a4afc0cd5f338556420
soumyasen10 <soumyasenofficial@gmail.com>
2025-01-19 20:29:55 +0530

    "Rename layout of user-app"

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -7,7 +7,7 @@ import { AppbarClient } from "../components/AppbarClient";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wallet",
+  title: "ZAYANLLET",
   description: "Simple wallet app",
 };
 
